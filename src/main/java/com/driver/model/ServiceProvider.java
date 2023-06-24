523fb6764855eae40f3d88adacd5b15fcc14653f
firdoushsiddique62 <firdoushsiddique62@gmail.com>
2023-06-24 15:21:05 +0530

    Done

---
+++ src/main/java/com/driver/model/ServiceProvider.java
@@ -0,0 +1,91 @@
+package com.driver.model;
+
+import org.apache.logging.log4j.util.PerformanceSensitive;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ServiceProvider {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "provide_name")
+    private String name;
+
+    // navigational properties
+    @ManyToOne
+    @JoinColumn
+    Admin admin;
+    @OneToMany(mappedBy = "serviceProvider",cascade = CascadeType.ALL)
+    List<Connection> connectionList=new ArrayList<>();
+
+    @OneToMany(mappedBy="serviceProvider",cascade = CascadeType.ALL)
+    List<Country> countryList=new ArrayList<>();
+
+    @ManyToMany(mappedBy = "serviceProviderList",cascade = CascadeType.ALL)
+    List<User> users= new ArrayList<>();
+
+    // getter and setter / constructor
+    public ServiceProvider(int id, String name, Admin admin, List<Connection> connections, List<Country> countries, List<User> users) {
+        this.id = id;
+        this.name = name;
+        this.admin = admin;
+        this.connectionList = connections;
+        this.countryList = countries;
+        this.users = users;
+    }
+
+    public ServiceProvider() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Admin getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Admin admin) {
+        this.admin = admin;
+    }
+
+    public List<Connection> getConnectionList() {
+        return connectionList;
+    }
+
+    public void setConnectionList(List<Connection> connections) {
+        this.connectionList = connections;
+    }
+
+    public List<Country> getCountryList() {
+        return countryList;
+    }
+
+    public void setCountryList(List<Country> countries) {
+        this.countryList = countries;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
---
+++ src/main/java/com/driver/model/User.java
@@ -0,0 +1,118 @@
+package com.driver.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+    private String originalIp;
+    private String maskedIp=null;
+    private Boolean connected=false;
+
+    // navigational properties
+    @ManyToMany
+    @JoinColumn
+    List<ServiceProvider> serviceProviderList=new ArrayList<>();
+
+    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL)
+    List<Connection> connectionList=new ArrayList<>();
+
+    @OneToOne(mappedBy = "user",cascade = CascadeType.ALL)
+    Country originalCountry;
+
+    // constructor and getter and setters
+
+    public User(int id, String username, String password, String originalIp, String maskedIp, boolean connected, List<ServiceProvider> serviceProviders, List<Connection> connections, Country country) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.originalIp = originalIp;
+        this.maskedIp = maskedIp;
+        this.connected = connected;
+        this.serviceProviderList = serviceProviders;
+        this.connectionList = connections;
+        this.originalCountry = country;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getOriginalIp() {
+        return originalIp;
+    }
+
+    public void setOriginalIp(String originalIp) {
+        this.originalIp = originalIp;
+    }
+
+    public String getMaskedIp() {
+        return maskedIp;
+    }
+
+    public void setMaskedIp(String maskedIp) {
+        this.maskedIp = maskedIp;
+    }
+
+    public boolean getConnected() {
+        return connected;
+    }
+
+    public void setConnected(Boolean connected) {
+        this.connected = connected;
+    }
+
+    public List<ServiceProvider> getServiceProviderList() {
+        return serviceProviderList;
+    }
+
+    public void setServiceProviderList(List<ServiceProvider> serviceProviders) {
+        this.serviceProviderList = serviceProviders;
+    }
+
+    public List<Connection> getConnectionList() {
+        return connectionList;
+    }
+
+    public void setConnectionList(List<Connection> connections) {
+        this.connectionList = connections;
+    }
+
+    public Country getOriginalCountry() {
+        return originalCountry;
+    }
+
+    public void setOriginalCountry(Country country) {
+        this.originalCountry= country;
+    }
+
+    public User(){}
+
+}
